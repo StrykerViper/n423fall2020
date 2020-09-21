8db815c79a09d694fbaac11cee279bcbec39eb9f
StrykerViper <StrykerViper@github.com>
2020-09-20 21:58:25 -0400

    edites

--- labs/HomeWork2/js/app.js
+++ labs/HomeWork2/js/app.js
@@ -1,8 +1,8 @@
 var apiKey = "7da4fa52d94d44ab8c4201938201409";
-var baseURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
+var baseURL = `//api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
 
 //stuff for homework
-var forcastBaseURL = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=`;
+var forcastBaseURL = `//api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=`;
 var forcastEndURL = `&days=3`;
 var storedZipOrCity = {};
 
